05cb4c958d43c2d139d6a7a5f792576a7de6d8fc
root <root@21d4e6d121ab>
2020-12-13 06:54:19 +0000

    Finish Riddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -13,9 +13,33 @@ void setup() {
     pepperoni.resize(30,30);
     mushroom = loadImage("mushroom.png");
     mushroom.resize(30,30);
+    olive = loadImage("olive.png");
+    olive.resize(30,30);
+    
 }
 void draw() {
+  if(mousePressed) {
+   
   image(pepperoni,200,200);
   image(mushroom,240,230);
+  image(olive,280,260);
+  image(pepperoni,250,260);
+  image(mushroom,230,270);
+  image(olive,220,290);
+  image(pepperoni,200,260);
+  image(mushroom,260,290);
+  image(olive,225,240);
+  image(pepperoni,210,210);
+   image(mushroom,290,260);
+   image(olive,210,290);
+   image(pepperoni,250,260);
+   image(mushroom,230,270);
+   image(olive,280,200);
+    image(mushroom,270,195);
+     image(pepperoni,290,220);
+     image(olive,240,210);
+     image(pepperoni,200,260);
+   
   
+  }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,26 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String rid = JOptionPane.showInputDialog("What has many keys and can't open a single lock?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(rid.equalsIgnoreCase("Piano")){
+JOptionPane.showMessageDialog(null,"Correct!");
+score +=1;	
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong, the answer was Piano.");
+	
+	
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
-	}
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
+	} 
 }
 
