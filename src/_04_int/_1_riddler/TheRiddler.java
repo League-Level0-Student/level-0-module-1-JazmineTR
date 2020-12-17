f75366e3a1b128c279dde43765dc7a3f1d6670d2
root <root@6be0411b9eee>
2020-12-17 23:26:33 +0000

    Finish Zombie EYE

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -19,19 +19,38 @@ String rid = JOptionPane.showInputDialog("What has many keys and can't open a si
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 if(rid.equalsIgnoreCase("Piano")){
 JOptionPane.showMessageDialog(null,"Correct!");
-score +=1;	
+score += 1;
 }
 else {
 	JOptionPane.showMessageDialog(null, "Wrong, the answer was Piano.");
-	
-	
 }
+String stomp = JOptionPane.showInputDialog("What can travel around the world while staying in a corner?");
+	
+if(stomp.equalsIgnoreCase("Stamp")) {
+	JOptionPane.showMessageDialog(null,"Correct!");
+	score += 1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "Incorrect, the answer was Stamp.");
+}
+	String numb = JOptionPane.showInputDialog("I am an odd number. Take away a letter and I become even. What number am I?");
+
+if(numb.equalsIgnoreCase("Seven")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score += 1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong, the answer was Seven.");
+}
+	
+
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
 		JOptionPane.showMessageDialog(null, "Your score is " + score);
-	} 
 }
+}
+
 
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,14 @@
-
+ PImage face; 
 void setup() {
+  size(500,600);
+face = loadImage("face.png");
+face.resize(500,600);
 
 }
 void draw() {
+image(face,0,0);
+fill(14,241,0);
+ellipse(230,198, 75,85);
+ellipse(330,220, 50, 75);
 
 }
